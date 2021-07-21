d5c5c6e3ce7ccc848d52eb9a60fc56bfcf6e12d8
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-07-21 17:27:35 +0700

    Set Union + Intersection - Dicoding

--- bin/Collections.dart
+++ bin/Collections.dart
@@ -5,4 +5,13 @@ void main() {
   List<String> arr2 = ["dim2"];
 
   print([...?arr1, ...arr2]); // Null aware
+
+  var setA = {1, 2, 4, 5};
+  var setB = {1, 5, 7};
+  
+  var union = setA.union(setB);
+  var intersection = setA.intersection(setB);
+  
+  print(union);
+  print(intersection);
 }
