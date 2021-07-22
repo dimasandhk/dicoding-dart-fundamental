b81ee510ebc7cab9dc332eec851c71e354c1bf1b
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-07-22 14:43:32 +0700

    Implicit Interface & Abstract Class

--- bin/Inheritance_OOP.dart
+++ bin/Inheritance_OOP.dart
@@ -1,10 +1,26 @@
-class Person {
+abstract class Person {
   String name;
   int age;
 
   Person(this.name, this.age);
 }
 
-class PersonHaveJob extends Person {
+class Driver {
+  void drive() {
+    print('Dari class driver');
+  }
+}
+
+class PersonHaveJob extends Person implements Driver {
   PersonHaveJob(String name, int age, String job) : super(name, age);
+
+  @override
+  drive() {
+    print('Dari class personhavejob');
+  }
+}
+
+void main() {
+  var Sandhika = new PersonHaveJob('Sandhika', 27, 'Driver');
+  Sandhika.drive();
 }
