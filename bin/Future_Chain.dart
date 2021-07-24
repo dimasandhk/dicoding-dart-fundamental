798c42d07208dc4f44539568cb3ada296a967e7e
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-07-24 11:26:57 +0700

    Asynchronous with async await

--- bin/Future_Chain.dart
+++ bin/Future_Chain.dart
@@ -1,17 +1,18 @@
-void main() {
-  getOrder().then((value) {
-    print('You order: $value');
-  }).catchError((error) {
-    print('Sorry. $error');
-  });
-  
+void main() async {
+  try {
+    var order = await getOrder();
+    print(order);
+  } catch (err) {
+    print(err);
+  }
+
   print('Getting your order...');
 }
-     
+
 Future<String> getOrder() {
   return Future.delayed(Duration(seconds: 3), () {
     var isStockAvailable = false;
 
-    return isStockAvailable ? 'Coffee Boba': throw 'Our stock is not enough';
+    return isStockAvailable ? 'Coffee Boba' : throw 'Our stock is not enough';
   });
 }
